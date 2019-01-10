f1f1bf2c70f15d4593a82c4c49096405d579277e
DrewDrizzle <aramouni8@gmail.com>
2019-01-09 20:41:32 -0500

    added files

---
+++ liri.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+// Client ID 5422b389d37b4b27a27dcb61c5659b97
+// Client Secret 45ea0d850a79406596d3599912128cfb
