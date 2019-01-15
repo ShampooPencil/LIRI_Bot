15daec3bf9c9546d6456077960fe6ec727a86f1e
DrewDrizzle <aramouni8@gmail.com>
2019-01-14 20:18:41 -0500

    added command lines

--- instruction.md
+++ instruction.md
@@ -1,32 +1,3 @@
-Before You Begin
-
-LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
-Make a new GitHub repository called liri-node-app and clone it to your computer.
-To retrieve the data that will power this app, you'll need to send requests using the axios package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
-
-Node-Spotify-API
-
-Axios
-
-You'll use Axios to grab data from the OMDB API and the Bands In Town API
-
-Moment
-DotEnv
-
-Submission Guide
-
-Create and use a standard GitHub repository. As this is a CLI App, it cannot be deployed to GitHub pages or Heroku. This time you'll need to include screenshots, a GIF, and/or a video showing us that you have the app working with no bugs. You can include these screenshots/GIFs or a link to a video in a README.md file.
-
-Include screenshots (or a GIF/Video) of the typical user flow of your application. Make sure to include the use of Spotify, Bands in Town, and OMDB.
-Include any other screenshots you deem necessary to help someone who has never been introduced to your application understand the purpose and function of it. This is how you will communicate to potential employers/other developers in the future what you built and why, and to show how it works.
-Because screenshots (and well-written READMEs) are extremely important in the context of GitHub, this will be part of the grading.
-
-
-If you haven't written a markdown file yet, click here for a rundown, or just take a look at the raw file of these instructions.
-
-Submission on BCS
-
-Please submit the link to the Github Repository!
 
 Instructions
 
@@ -88,15 +59,10 @@ spotify-this-song
 movie-this
 do-what-it-says
 
-
-
 What Each Command Should Do
 
 
 node liri.js concert-this <artist/band name here>
-
-
-
 
 This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
 
@@ -105,17 +71,9 @@ Name of the venue
 Venue location
 Date of the Event (use moment to format this as "MM/DD/YYYY")
 
-
-
-
-
 node liri.js spotify-this-song '<song name here>'
 
-
-
-
 This will show the following information about the song in your terminal/bash window
-
 
 Artist(s)
 The song's name
@@ -131,12 +89,7 @@ Step Two: Either login to your existing Spotify account or create a new one (a f
 Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
 Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
 
-
-
 node liri.js movie-this '<movie name here>'
-
-
-
 
 This will output the following information to your terminal/bash window:
 
@@ -149,32 +102,19 @@ This will output the following information to your terminal/bash window:
    * Plot of the movie.
    * Actors in the movie.
 
-
 If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 
-
 If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-
 It's on Netflix!
-
 
 You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
 
-
-
 node liri.js do-what-it-says
-
-
-
 
 Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 
-
 It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
 Edit the text in random.txt to test out the feature for movie-this and concert-this.
-
-
-
 
 
 BONUS
--- liri.js
+++ liri.js
@@ -1,70 +1,130 @@
 
-//********************* */ 1/9/19 UPDATE: So tomorrow, in the morning, start
-// a pseucode on this HW; I think it would be very important for me
-// and start a good foundation for the app. (I think the foundation is inquirer) also start 
-// really reading the instructions so i can have a good grasp of what im doing 
-// (actually first then do pseucode it out!!!!!*************************)
-
 // Client ID 5422b389d37b4b27a27dcb61c5659b97
 // Client Secret 45ea0d850a79406596d3599912128cfb
 var moment = require('moment');
-var inquirer = require("inquirer");
+//var inquirer = require("inquirer");
 moment().format();
 require("dotenv").config();
-var http = require("http");
-var keys = require("./keys.js");
+var http = require("http"); //import npm
+var keys = require("./keys.js"); //import file instead of npm
+var Spotify = require('node-spotify-api');
+var request = require("request");
+var fs = require('fs');
 //var spotify = new Spotify(keys.spotify);
 /* Load the HTTP library */
 
+var thirdCommand = process.argv[2];
+var fourthCommand = process.argv.slice(3).join(" ");
 
-// /* Create an HTTP server to handle responses */
+var startSpotify = function(songName){
+  console.log(keys.spotify.id);
+  console.log(keys.spotify.secret);
+  var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
+  
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   for(var i = 0; i < data.tracks.items.length; i++){
+     console.log(data.tracks.items[i].album.name);    ///going to loop through all the tracks and grab specific information
+     
+   }
+   if (thirdCommand === 'concert-this'){
+    var artist = fourthCommand;
+    console.log(artist);
+    var queryUrl = "https://rest.bandsintown.com/artists/"+artist+"/events?app_id=codecademy";
+    request(queryUrl, function(err, response, data) { // cant have a response back its annoying
+      // console.log(data);
+      var formattedData = JSON.parse(data);
+      var dateTime = moment(formattedData[0].datetime).format("MM/DD/YYYY");
+      console.log(
+          "Venue: " + formattedData[0].venue.name + "\nLocation: " + formattedData[0].venue.city + "\nDate: " + dateTime
+        );
+      });
 
-// http.createServer(function (request, response) {
-//     response.writeHead(200, { "Content-Type": "text/plain" });
-//     response.write("Hello World");
-//     response.end();
-// }).listen(8888);
-// Create a "Prompt" with a series of questions.
-var inquirerFunction = function(){
-inquirer
-  .prompt([
-    // Here we create a basic text prompt.
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "username"
-    },
-    // Here we create a basic password-protected text prompt.
-    {
-      type: "password",
-      message: "Set your password",
-      name: "password"
-    },
-    // Here we give the user a list to choose from.
-    {
-      type: "list",
-      message: "Which Pokemon do you choose?",
-      choices: ["Bulbasaur", "Squirtle", "Charmander"],
-      name: "pokemon"
-    },
-    // Here we ask the user to confirm.
-    {
-      type: "confirm",
-      message: "Are you sure:",
-      name: "confirm",
-      default: true
+  // IF/ELSE IF STATEMENTS ONE-AT-A-TIME****  ***********!!!!!!!!!********
+      }else if (thirdCommand === 'spotify-this-song'){
+        var Song = fourthCommand;
+        var searchTrack;
+        if (Song === undefined) {
+          searchTrack = "The Sign ace of base";
+        }else {
+          searchTrack = Song;
+        }
+        spotify.search({
+          type: 'track',
+          query: searchTrack
+        }, function(error, data) {
+          if (error) {
+            logIt('Error occurred: ' + error);
+            return;
+          } else {
+            console.log("\n---------------------------------------------------\n");
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Preview: " + data.tracks.items[0].href);
+            console.log("Album: " + data.tracks.items[0].album.name);
+            
+            console.log("\n---------------------------------------------------\n");
+            
+          }
+        });
+    
+      }
+
+    else if (thirdCommand === 'movie-this'){
+      var movie = fourthCommand;
+
+      var queryUrl = ' http://www.omdbapi.com/?i=tt3896198&apikey=babebd84&t='+movie; //more data,add ,more query parameters t for omdb just for the value/title
+      request(queryUrl, function(err, response, data) { // cant have a response back its annoying
+        // console.log(data);
+        var formattedData = JSON.parse(data); // 
+        //var dateTime = moment(formattedData[0].datetime).format("MM/DD/YYYY");
+        console.log("Title: " + formattedData.Title);//switched to formattedData because turned this any value to a string on line 83
+        console.log("Year: " + formattedData.Year);
+        console.log("imdbRating: " + formattedData.imdbRating);
+        console.log("Rotten Tomatoes: " + formattedData["Rotten Tomatoes"]);
+        console.log("Country: " + formattedData.Country);
+        console.log("Language: " + formattedData.Language);
+        console.log("Plot: " + formattedData.Plot);
+        console.log("Actors: " + formattedData.Actors);
+
+
+
+        });
+        // * Title of the movie.
+        // * Year the movie came out.
+        // * IMDB Rating of the movie.
+        // * Rotten Tomatoes Rating of the movie.
+        // * Country where the movie was produced.
+        // * Language of the movie.
+        // * Plot of the movie.
+        // * Actors in the movie.
+     
     }
-  ])
-  .then(function(inquirerResponse) {
-    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-    if (inquirerResponse.confirm) {
-      console.log("\nWelcome " + inquirerResponse.username);
-      console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
-    }
-    else {
-      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
-    }
+
+  else if (thirdCommand === 'do-what-it-says'){
+     fs.readFile('random.txt', 'utf8', function(err,data){
+       console.log(data);
+       var arguments = data.split(',');
+       console.log(arguments[0]);
+       console.log(arguments[1]);
+     }); //basically pulled data from the file
+
+//tip..maybe put this in a function
+
+  }else {
+    console.log("Try Again");
+  }
+  
+  //console.log(data); 
   });
 }
-inquirerFunction();
+startSpotify("Juicy");
+
+
+
 
